36408760973d550a315443eeb25d762bbad5cb11
Jiayuan Zhu <jiayuanz0322@gmail.com>
2022-10-20 11:31:24 +0100

    pass provided cases

--- pkmodel/visualisation.py
+++ pkmodel/visualisation.py
@@ -6,7 +6,7 @@ import numpy as np
 
 class visualisation():
     def __init__(self, model1, model2=None):
-        self.models = [self.model1, self.model2]
+        self.models = [model1, model2]
 
 
     def plot_figure(self):
@@ -20,8 +20,9 @@ class visualisation():
         linestyle_list = ['-', '--', ':']
 
         # legend list 
-        legend_list = ['Central Compartment', 'Peripheral Compartment1', 
-                        'Peripheral Compartment2']
+        legend_list = ['C', 'P1', 'P2']
+
+        colour_list = ['red', 'blue']
 
         # only one figure
         fig = plt.figure()
@@ -33,11 +34,11 @@ class visualisation():
             # consider more than one dose
             for j in range(0, len(self.models[i])):
                 # different linestyles according to number of systems
-                for k in range(0, len(self.models[i].y)):
+                for k in range(0, len(self.models[i][j].y)):
                     plt.plot(self.models[i][j].t, self.models[i][j].y[k, :], 
-                    label=f'model_{i+1}-'+ f'mass in{legend_list[k]}', 
+                    label=f'model_{i+1}-'+ f'mass in {legend_list[k]}', 
                     linestyle = linestyle_list[k], 
-                    color = plt.rcParams["axes.prop_cycle"][i])
+                    color = colour_list[i])
 
         plt.legend()
         plt.title('Drug Mass [ng] versus Time [h]')
@@ -45,3 +46,4 @@ class visualisation():
         plt.xlabel('time [h]')
         #plt.savefig('../Figure/Drug Mass [ng] versus Time [h].png')
         plt.show()
+
