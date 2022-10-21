054e6ce09ee62a20a46b280dcaf1e7b1831233e9
arunraja-hub <surviva614@gmail.com>
2022-10-21 15:55:07 +0100

    read the docs

--- pharmodel/model.py
+++ pharmodel/model.py
@@ -23,12 +23,21 @@ class Model:
    
 
     def __init__(self, absorb, comp, V_c, CL, Q_p1 = 0, V_p1 = 0.1, Q_p2 = 0, V_p2 = 0.1, k_a = 0):
-        self.absorb = absorb
+        # if absorb == 0 or 1:
+        #     self.absorb = absorb
+        # else:
+         #   raise ValueError('absorb should either take the value of 0 or 1')
+
+        self.absorb = absorb 
         self.comp = comp
-        self.V_c = V_c
-        self.CL = CL
-        self.Q_p1 = Q_p1
-        self.V_p1 = V_p1
-        self.Q_p2 = Q_p2
-        self.V_p2 = V_p2
-        self.k_a = k_a
+        self.V_c = V_c if (V_c > 0) else self.raiseValueErr('V_c should be greater than 0')
+        self.CL = CL if (CL > 0) else self.raiseValueErr('CL should be greater than 0')
+        self.Q_p1 = Q_p1 if (Q_p1 >= 0) else self.raiseValueErr('Q_p1 should be greater than or equal to 0')
+        self.V_p1 = V_p1 if (V_p1 >= 0) else self.raiseValueErr('V_p1 should be greater than or equal to 0')
+        self.Q_p2 = Q_p2 if (Q_p2 >= 0) else self.raiseValueErr('Q_p2 should be greater than or equal to 0')
+        self.V_p2 = V_p2 if (V_p2 >= 0) else self.raiseValueErr('V_p2 should be greater than or equal to 0')
+        self.k_a = k_a if (k_a >= 0) else self.raiseValueErr('k_a should be greater than or equal to 0')
+
+    def raiseValueErr(self,stringexp):
+        raise ValueError(stringexp)
+
--- pharmodel/tests/test_protocol.py
+++ pharmodel/tests/test_protocol.py
@@ -1,5 +1,8 @@
 import unittest
+import pytest
 import pkmodel as pk
+import numpy as np
+from pharmodel.protocol import Protocol
 
 
 class ProtocolTest(unittest.TestCase):
@@ -12,4 +15,20 @@ class ProtocolTest(unittest.TestCase):
         """
         model = pk.Protocol()
         self.assertEqual(model.value, 43)
+
+    # @pytest.mark.parametrize("test, expected",
+    # [( "test_dosis_constant_negative.csv", Protocol("test_dosis_constant_positive.csv"))
+
+    # ])
+
+    # def test_read_dosage(test, expected):
+    #     """Test protocol could convert csv files to pandas dataframe"""
+        
+    #     # if expected_error:
+    #     #     with pytest.raises(expected_error):
+    #     #         np.assert_array_equal(expected, Protocol(test).read_dosage())
+    #     # else:
+    #     test = Protocol(test).read_dosage()
+    #     assert np.all(len(test.select_dtypes(include=["float", 'int']).columns) \
+    #         == len(test.columns))
 
