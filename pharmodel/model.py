c8ea8627ec7ac17746df38c15fbd5343aa80b5de
Jiayuan Zhu <jiayuanz0322@gmail.com>
2022-10-21 14:22:15 +0100

    flake8 check

--- pharmodel/model.py
+++ pharmodel/model.py
@@ -4,25 +4,26 @@
 
 class Model:
     """A Pharmokinetic (PK) model
-    This class acts as a medium to accept the user inputs for the pharmokinetic model
+    This class acts as a medium to accept the user
+    inputs for the pharmokinetic model
 
     Parameters
     ----------
 
-    absorb: numeric, binary value where 1 means there is an absoption compartment and 0 vice versa
+    absorb: numeric, binary value where 1 means there is an
+    absoption compartment and 0 vice versa
     comp: numeric, number of peripheral compartments
-    V_c:  
-    CL: 
+    V_c:
+    CL:
     Q_p1:
     V_p1:
     Q_p2:
     V_p2:
     k_a:
         """
-    
-   
 
-    def __init__(self, absorb, comp, V_c, CL, Q_p1 = 0, V_p1 = 0.1, Q_p2 = 0, V_p2 = 0.1, k_a = 0):
+    def __init__(self, absorb, comp, V_c, CL, Q_p1=0, V_p1=0.1,
+                 Q_p2=0, V_p2=0.1, k_a=0):
         self.absorb = absorb
         self.comp = comp
         self.V_c = V_c
